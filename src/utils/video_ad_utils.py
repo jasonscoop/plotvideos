2f3e6a8413597c40b077a4f0ad63b4426d1f27ad
Gary Meng <guozao.m@gmail.com>
2025-05-30 15:47:25 +0400

    Add StartAdTrimmer

--- src/utils/video_ad_utils.py
+++ src/utils/video_ad_utils.py
@@ -1,18 +1,26 @@
-import os
 import re
 import subprocess
 from pathlib import Path
 
 import ffmpeg
-import pytesseract
-from PIL import Image
 
 
-class VideoUtils:
-    @staticmethod
-    def detect_scene_change(video_path, scene_threshold=0.4):
+class StartAdTrimmer:
+    def __init__(self, video_path: Path):
+        self.video_path: Path = video_path
+        self.new_video_path: Path = video_path.parent / f"{video_path.stem}_trimmed{video_path.suffix}"
+
+    def trim_ad(self) -> Path:
+        start = self.detect_scene_change()
+        if start != 0.0:
+            self.trim_video(start)
+            return self.new_video_path
+
+        return self.video_path
+
+    def detect_scene_change(self, scene_threshold=0.4):
         # Build the ffmpeg command using ffmpeg-python
-        stream = ffmpeg.input(video_path)
+        stream = ffmpeg.input(self.video_path)
         stream = stream.video.filter('select', f'gt(scene,{scene_threshold})').filter('showinfo')
         cmd = ffmpeg.output(stream, 'null', f='null').compile()
 
@@ -25,6 +33,5 @@ class VideoUtils:
             return float(matches[0])  # First major scene change time
         return 0.0
 
-    @staticmethod
-    def trim_video(input_path, output_path, start_time):
-        ffmpeg.input(input_path, ss=start_time).output(output_path, c='copy').run()
+    def trim_video(self, start_time):
+        ffmpeg.input(self.video_path.as_posix(), ss=start_time).output(self.new_video_path.as_posix(), c='copy').run()

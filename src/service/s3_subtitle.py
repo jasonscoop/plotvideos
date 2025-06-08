7f90935605f7895b1dfa4ab66bebd79d3460549d
Gary Meng <guozao.m@gmail.com>
2025-06-08 13:16:18 +0400

    Use multi-threading to subtitle

--- src/service/s3_subtitle.py
+++ src/service/s3_subtitle.py
@@ -1,5 +1,6 @@
 import sys
 import traceback
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from loguru import logger
 
@@ -10,6 +11,32 @@ from src.lib.models import VideoStatus
 from src.utils.azure_subtitle_utils import generate_subtitle
 from src.utils.log_utils import init_logging
 from src.utils.string_utils import get_tokens
+
+
+def subtitle_video(video):
+    if video.file_size > MAX_ACCEPT_VIDEO_SIZE:
+        reason = f"[{video.id} | {video.host} | {video.original_id}] size exceeded: {MAX_ACCEPT_VIDEO_SIZE}"
+        VideoCrud.update_status(video.id, VideoStatus.skipped_due_to_size, reason)
+        logger.info(reason)
+        return None
+
+    try:
+        subtitle_content = generate_subtitle(video)
+        tokens = get_tokens(subtitle_content)
+        VideoCrud.update({
+            "id": video.id,
+            "subtitle_content": subtitle_content,
+            "subtitle_tokens": tokens,
+            "subtitle_duration_ratio": round(tokens / video.duration, 2),
+            "status": VideoStatus.subtitled
+        })
+        logger.info(f"[{video.id} | {video.host} | {video.original_id}] subtitle generated")
+        return None
+    except Exception as e:
+        reason = str(e)[:DB_ERROR_LOG_LENGTH]
+        VideoCrud.update_status(video.id, VideoStatus.failed_subtitled, reason)
+        traceback.print_exc()
+        return e
 
 
 def subtitle_videos(batch_size: int = 10, host: str = ""):
@@ -23,32 +50,14 @@ def subtitle_videos(batch_size: int = 10, host: str = ""):
 
         last_id = videos[-1].id
 
-        for video in videos:
-            if video.file_size > MAX_ACCEPT_VIDEO_SIZE:
-                reason = f"[{video.id} | {video.host} | {video.original_id}] size exceeded: {MAX_ACCEPT_VIDEO_SIZE}"
-                VideoCrud.update_status(video.id, VideoStatus.skipped_due_to_size, reason)
-                logger.info(reason)
-                continue
-
-            try:
-                subtitle_content = generate_subtitle(video)
-                tokens = get_tokens(subtitle_content)
-                VideoCrud.update({
-                    "id": video.id,
-                    "subtitle_content": subtitle_content,
-                    "subtitle_tokens": tokens,
-                    "subtitle_duration_ratio": round(tokens / video.duration, 2),
-                    "status": VideoStatus.subtitled
-                })
-                logger.info(
-                    f"[{video.id} | {video.host} | {video.original_id}] subtitle generated")
-            except Exception as e:
-                reason = str(e)[:DB_ERROR_LOG_LENGTH]
-                VideoCrud.update_status(video.id, VideoStatus.failed_subtitled, reason)
-                exception_count += 1
-                if exception_count >= 3:
-                    raise e
-                traceback.print_exc()
+        with ThreadPoolExecutor(max_workers=len(videos)) as executor:
+            futures = [executor.submit(subtitle_video, video) for video in videos]
+            for future in as_completed(futures):
+                error = future.result()
+                if error:
+                    exception_count += 1
+                    if exception_count >= 3:
+                        raise error
 
 
 if __name__ == '__main__':

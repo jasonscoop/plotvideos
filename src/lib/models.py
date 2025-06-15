59754091d73b0fa3aa6a6ac5b6dc9133ad12114d
Gary Meng <guozao.m@gmail.com>
2025-06-15 22:28:11 +0400

    remove tag_translations and category_translations

--- src/lib/models.py
+++ src/lib/models.py
@@ -43,8 +43,6 @@ class Video(Base):
     duration = Column(Integer, nullable=False, default=0)
 
     title_translations = Column(JSON, nullable=False, default={})
-    tag_translations = Column(JSON, nullable=False, default={})
-    category_translations = Column(JSON, nullable=False, default={})
 
     subtitle_content = Column(Text, nullable=False, default="")
     subtitle_tokens = Column(Integer, nullable=False, default=0)

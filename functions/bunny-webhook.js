0b223d5c7a9e329cd50c7b585f8962c9ae54474f
Gary Meng <guozao.m@gmail.com>
2025-06-17 09:57:54 +0400

    Set updated_at for videos

--- functions/bunny-webhook.js
+++ functions/bunny-webhook.js
@@ -1,12 +1,14 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-Deno.serve(async (req)=>{
-  const { VideoGuid, Status } = await req.json();
-  console.log(VideoGuid, Status);
-  if (Status === 3) {
-    const supabase = createClient(Deno.env.get('SUPABASE_URL'), Deno.env.get('SUPABASE_SERVICE_ROLE_KEY'));
-    await supabase.from('videos').update({
-      status: 'published'
-    }).eq('bunny_video_id', VideoGuid);
-  }
-  return new Response('OK');
+import {createClient} from 'https://esm.sh/@supabase/supabase-js@2';
+
+Deno.serve(async (req) => {
+    const {VideoGuid, Status} = await req.json();
+    console.log(VideoGuid, Status);
+    if (Status === 3) {
+        const supabase = createClient(Deno.env.get('SUPABASE_URL'), Deno.env.get('SUPABASE_SERVICE_ROLE_KEY'));
+        await supabase.from('videos').update({
+            status: 'published',
+            updated_at: new Date().toISOString()
+        }).eq('bunny_video_id', VideoGuid);
+    }
+    return new Response('OK');
 });

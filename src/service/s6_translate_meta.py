e1b00a662ad361fe81a6fb5a346391aa5c1f3a21
Gary Meng <guozao.m@gmail.com>
2025-08-31 22:57:13 +0400

    format the code

--- src/service/s6_translate_meta.py
+++ src/service/s6_translate_meta.py
@@ -22,7 +22,9 @@ def translate_video(video: Video, languages):
 
     for lang in languages:
         existing_translations = all_translations.get(lang.code, {})
-        terms_to_translate = [term for term in all_terms if term not in existing_translations]
+        terms_to_translate = [
+            term for term in all_terms if term not in existing_translations
+        ]
 
         # Combine title and new terms for a single translation request
         texts_to_translate = [video.title] + terms_to_translate
@@ -30,22 +32,27 @@ def translate_video(video: Video, languages):
         if texts_to_translate:
             try:
                 new_translations = translate_texts1(texts_to_translate, lang)
-                logger.info(f"[{video.id} | {video.host} | {video.original_id}] translated with translator1")
+                logger.info(
+                    f"[{video.id} | {video.host} | {video.original_id}] translated with translator1"
+                )
             except HTTPError as e:
                 new_translations = translate_texts2(texts_to_translate, lang)
                 logger.warning(
-                    f"[{video.id} | {video.host} | {video.original_id}] translated with translator2 (fallback)")
+                    f"[{video.id} | {video.host} | {video.original_id}] translated with translator2 (fallback)"
+                )
 
         title_translations[lang.code] = new_translations[0]
         for term, translation in zip(terms_to_translate, new_translations[1:]):
             TermCrud.create(term, lang.code, translation)
 
-    VideoCrud.update({
-        "id": video.id,
-        "title_translations": title_translations,
-        "status": VideoStatus.meta_translated,
-        "failed_reason": "",
-    })
+    VideoCrud.update(
+        {
+            "id": video.id,
+            "title_translations": title_translations,
+            "status": VideoStatus.meta_translated,
+            "failed_reason": "",
+        }
+    )
 
 
 def translate_meta_infos(host: str = ""):
@@ -54,7 +61,9 @@ def translate_meta_infos(host: str = ""):
     languages = LanguageCrud.get_all()
 
     while True:
-        videos = VideoCrud.batch_get(last_id, S6_TRANSLATE_META_BATCH_SIZE, VideoStatus.vtt_translated, host)
+        videos = VideoCrud.batch_get(
+            last_id, S6_TRANSLATE_META_BATCH_SIZE, VideoStatus.vtt_translated, host
+        )
         if not videos:
             logger.info("All meta translated, sleeping for 5 minutes")
             time.sleep(5 * 60)
@@ -66,9 +75,13 @@ def translate_meta_infos(host: str = ""):
         for video in videos:
             try:
                 translate_video(video, languages)
-                logger.info(f"[{video.id} | {video.host} | {video.original_id}] translated")
+                logger.info(
+                    f"[{video.id} | {video.host} | {video.original_id}] translated"
+                )
             except Exception as e:
-                VideoCrud.update_status(video.id, VideoStatus.failed, VideoStatus.meta_translated.log(e))
+                VideoCrud.update_status(
+                    video.id, VideoStatus.failed, VideoStatus.meta_translated.log(e)
+                )
                 exception_count += 1
                 traceback.print_exc()
                 rm_video(video)
